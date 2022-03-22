5bb9f57c9c6fc28b4d8872ff24b26c9358eb443a
Richard Cranwell <rickycranwell@gmail.com>
2022-03-22 17:12:15 -0500

    display error to console if card not found

--- src/logic/deckPricer.js
+++ src/logic/deckPricer.js
@@ -12,22 +12,34 @@ async function priceDeck(importText) {
   // process cards one by one
   parsed.cards.forEach((card) => {
     const ptcgioURL = `https://api.pokemontcg.io/v2/cards/${card.ptcgoio.id}`;
-    const newPromise = new Promise(async (resolve)=>{
+    // eslint-disable-next-line no-async-promise-executor
+    const newPromise = new Promise(async (resolve) => {
       const response = await fetch(ptcgioURL);
       const data = await response.json();
-      const newCard = { ...card };
-      newCard.rarity = data.data.rarity;
-      newCard.costPerCopy = craftingCosts[newCard.rarity];
-      newCard.totalCost = newCard.costPerCopy * newCard.amount;
-      resolve(newCard);
-    })
+      // console.log(data);
+      if (data.error) {
+        // console.error(data.error.message);
+        console.error('Card not found:', card.name, card.set, card.code);
+        const newCard = { ...card };
+        newCard.rarity = null;
+        newCard.costPerCopy = 0;
+        newCard.totalCost = 0;
+        newCard.notFound = true;
+      } else {
+        const newCard = { ...card };
+        newCard.rarity = data.data.rarity;
+        newCard.costPerCopy = craftingCosts[newCard.rarity];
+        newCard.totalCost = newCard.costPerCopy * newCard.amount;
+        resolve(newCard);
+      }
+    });
     promiseArray.push(newPromise);
   });
 
   const dataArray = await Promise.all(promiseArray);
-  dataArray.forEach(card=>{
+  dataArray.forEach((card) => {
     total += card.totalCost;
-  })
+  });
 
   return {
     total,
