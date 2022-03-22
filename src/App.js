3952a0ea42ce375b87f05fbf4fd8143a12f540d5
Richard Cranwell <rickycranwell@gmail.com>
2022-03-22 17:51:15 -0500

    make simple display for decklist pricer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import deckPricer from './logic/deckPricer';
+
+function onSubmit(e, setPriceData) {
+  e.preventDefault();
+  console.log(e);
+  const textarea = e.target.form[0];
+  console.log(textarea.value);
+
+  deckPricer.priceDeck(textarea.value).then((result) => {
+    setPriceData(result);
+  });
+}
 
 function App() {
+  const [priceData, setPriceData] = useState({});
+
   return (
     <div className="App">
-      My App
+      <form>
+        <textarea placeholder="Export your decklist here." />
+        <button type="submit" onClick={(e) => { onSubmit(e, setPriceData); }}>Price your deck!</button>
+      </form>
+      {Number.isInteger(priceData.total)
+      && (
+      <div>
+        <div>
+          {priceData.cards.map((card, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div key={index}>{card.name}</div>
+          ))}
+        </div>
+        <div>
+          Total:
+          {' '}
+          {priceData.total}
+          {' '}
+          Credits
+        </div>
+      </div>
+      )}
     </div>
   );
 }
--- src/logic/deckPricer.js
+++ src/logic/deckPricer.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import ptcgoParser from './ptcgoParser.js';
 import craftingCosts from './craftingCosts.js';
 
